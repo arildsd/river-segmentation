2608564722092fb0b046e743535e94c941e65930
arildsd <arildsd@stud.ntnu.no>
2019-10-28 14:26:13 +0100

    Added a function to rasterize polygons.

--- source/data_processing.py
+++ source/data_processing.py
@@ -20,24 +20,53 @@ def find_intersecting_polys(geometry, polys):
     return intersecting_polys
 
 
+def rasterize_polygons(polygons, image_path, class_name, driver=gdal.GetDriverByName("GTiff")):
+    """
+    Retuns a data set image with the bounding box dimensions and the polygon locations marked by 1.
+    :param polygons: A list of polygons of the same class
+    :param bounding_box: A bounding box. type: Geometry
+    :return: Data set image
+    """
+    # Get meta data from the image
+    image_ds = gdal.Open(image_path)
+    geo_transform = image_ds.GetGeoTransform()
+    projection = image_ds.GetProjection()
+    n_pixels_north = image_ds.RasterYSize()
+    n_pixels_east = image_ds.RasterXSize()
+
+    # Create empty image
+    output_path = image_path.remove(".tif")
+    output_path += f"_temp_label_{class_name}.tif"
+    label_raster = driver.Create(output_path, n_pixels_east, n_pixels_north,
+                  1, gdal.GetDataTypeByName("int16"))
+    label_raster.SetGeoTransform(geo_transform)
+    label_raster.SetProjection(projection)
+
+    # Burn the polygons into the new image
+    for poly in polys:
+        gdal.Rasterize(label_raster, poly)
+    # Save and close the files
+    image_ds = None
+
+    return label_raster
+
+
 def create_raster_labels(image_path, polys):
     # Create bounding box for the image
-    rectangle = create_bounding_box(image_path)
+    bounding_box = create_bounding_box(image_path)
     # Find intersecting polygons
-    intersecting_polys = find_intersecting_polys(rectangle, polys)
+    intersecting_polys = find_intersecting_polys(bounding_box, polys)
     #TODO: transform geometries to np arrays
     #TODO: figure out the post-processing on the geometries
     pass
+
 
 def create_bounding_box(image_path):
     # Create a bounding box geometry for the image
     image_ds = gdal.Open(image_path)
     geo_transform = image_ds.GetGeoTransform()
-    band = image_ds.GetRasterBand(1)
-    matrix = band.ReadAsArray()
-    shape = matrix.shape
-    n_pixels_north = shape[0]
-    n_pixels_east = shape[1]
+    n_pixels_north = image_ds.RasterYSize()
+    n_pixels_east = image_ds.RasterXSize()
     top_left_coordinate = (geo_transform[0], geo_transform[3])
     bottom_right_coordinate = (geo_transform[0] - n_pixels_north * geo_transform[1],
                                geo_transform[3] - geo_transform[5] * n_pixels_east)
@@ -48,6 +77,8 @@ def create_bounding_box(image_path):
     ring.AddPoint(bottom_right_coordinate[0], top_left_coordinate[1])  # Bottom left
     rectangle = ogr.Geometry(ogr.wkbPolygon)
     rectangle.AddGeometry(ring)
+    return rectangle
+
 
 def load_polygons(filepath):
     polygons = []
@@ -66,6 +97,7 @@ def load_polygons(filepath):
             poly.AssignSpatialReference(reference)
             polygons.append(poly)
     return polygons
+
 
 if __name__ == '__main__':
     gdal.UseExceptions()
